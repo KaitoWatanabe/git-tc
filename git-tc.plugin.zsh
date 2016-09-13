a3a0a9e8863010e2d7c569cf545dd7a54fd020c6
kaitowatanabe <kalbus1892@gmail.com>
2016-09-13 15:25:59 +0900

    Add init command

--- git-tc.plugin.zsh
+++ git-tc.plugin.zsh
@@ -1,5 +1,18 @@
 function git-tc() {
-  TESTFILE=./$(git rev-parse --show-cdup).git-tc.txt
-  BUFFER=$(cat $TESTFILE | peco --query "$LBUFFER")
-  print -z "git commit -m \"$(echo $BUFFER | sed -e 's/#[^#]*#//g')\""
+  GITTCFILE=./$(git rev-parse --show-cdup).git-tc.txt
+  if [ "$1" != "" ]; then
+    if [ $1 = init ]; then
+      if [ ! -f $GITTCFILE ]; then
+        touch $GITTCFILE
+        echo -e "test" >> $GITTCFILE
+      else
+        echo "file exists"
+      fi
+    else
+      echo "$1 is not found"
+    fi
+  else
+    BUFFER=$(cat $GITTCFILE | peco --query "$LBUFFER")
+    print -z "git commit -m \"$(echo $BUFFER | sed -e 's/#[^#]*#//g')\""
+  fi
 }
