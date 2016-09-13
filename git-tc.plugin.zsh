fb228da5210cb23b2e4f9aec26180a120e26a0d5
kaitowatanabe <kalbus1892@gmail.com>
2016-09-13 16:16:27 +0900

    Add WPI

--- git-tc.plugin.zsh
+++ git-tc.plugin.zsh
@@ -1,18 +1,34 @@
 function git-tc() {
   GITTCFILE=./$(git rev-parse --show-cdup).git-tc.txt
   if [ "$1" != "" ]; then
-    if [ $1 = init ]; then
-      if [ ! -f $GITTCFILE ]; then
-        touch $GITTCFILE
-        echo -e "test" >> $GITTCFILE
-      else
-        echo "file exists"
-      fi
-    else
-      echo "$1 is not found"
-    fi
+    case "$1" in
+      "init" )  git-tc-init ;;
+      "-b" ) git-tc-default "branch: $(git_current_branch)" ;;
+      "-p" ) git-tc-default "[WIP]" ;;
+      * ) echo "$1 is not found" ;;
+    esac
   else
+    git-tc-default ''
+  fi
+}
+
+function git-tc-init() {
+  GITTCFILE=./$(git rev-parse --show-cdup).git-tc.txt
+
+  if [ ! -f $GITTCFILE ]; then
+    cp ~/.oh-my-zsh/custom/plugins/git-tc/.git-tc.txt.example $GITTCFILE
+    # echo -e $(cat ~/.oh-my-zsh/custom/plugins/git-tc/.git-tc.txt.example) >> $GITTCFILE
+  else
+    echo "file exists"
+  fi
+}
+
+function git-tc-default() {
+  GITTCFILE=./$(git rev-parse --show-cdup).git-tc.txt
+  if [ -f $GITTCFILE ]; then
     BUFFER=$(cat $GITTCFILE | peco --query "$LBUFFER")
-    print -z "git commit -m \"$(echo $BUFFER | sed -e 's/#[^#]*#//g')\""
+    print -z "git commit -m \"$1 $(echo $BUFFER | sed -e 's/#[^#]*#//g')\" "
+  else
+    echo ".git-tc.text not found. please command \`git-tc init\`"
   fi
 }
