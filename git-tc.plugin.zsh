6d784de4159c4b5c54ea49314c507eb9ccecf65c
kaitowatanabe <kalbus1892@gmail.com>
2016-09-13 14:51:36 +0900

    Add regexp

--- git-tc.plugin.zsh
+++ git-tc.plugin.zsh
@@ -1,5 +1,5 @@
 function git-tc() {
   TESTFILE=./$(git rev-parse --show-cdup).git-tc.txt
   BUFFER=$(cat $TESTFILE | peco --query "$LBUFFER")
-  print -z "git commit -m \"$(echo $BUFFER)\""
+  print -z "git commit -m \"$(echo $BUFFER | sed -e 's/#[^#]*#//g')\""
 }
